91687c087e00a559d509547121e26fb9c17b0bc3
alkich <alkich@yandex.ru>
2012-03-14 16:55:04 +0400

    Version 0.9.2

---
+++ PHPDocx_0.9.2.php
@@ -0,0 +1,195 @@
+<?php
+
+/**************************************
+/
+/  *** PHP Library PHPDocx ***
+/  Version: 0.9.2
+/  Author: Alexey Kichaev
+/  Home page: http://webli.ru/phpdocx/
+/  License: MIT or GPL
+/  GITHub: https://github.com/alkich/PHPDocx
+/  Date: 14/03/2012
+/
+/***************************************/
+
+// Общий класс для создания генераторов MS Office документов
+class OfficeDocument extends ZipArchive{
+
+    // Файлы для включения в архив
+    protected $files;
+
+    // Путь к шаблону
+    protected $path;
+
+    // Содержимое документа
+    protected $content;
+
+    // Множитель для перевода размеров изображений из пикселей в EMU
+    protected $px_emu = 8625;
+
+    // Делаем приватно, чтобы не было возможности вшить дрянь в документ
+    protected $rels = array();
+
+    public function __construct($filename, $template_path = '/template/' ){
+
+      // Путь к шаблону
+      $this->path = dirname(__FILE__) . $template_path;
+
+      // Если Вы на windows - имена файлов должны быть под windows-1251
+      $filename = iconv( 'utf-8', 'windows-1251', $filename );
+
+      // Если не получилось открыть файл, то жизнь бессмысленна.
+      if ( $this->open( $filename, ZIPARCHIVE::CREATE) !== TRUE) {
+        die("Unable to open <$filename>\n");
+      }
+
+      // Описываем связи для документа MS Office
+      $this->rels = array_merge(array(
+        'rId3' => array(
+          'http://schemas.openxmlformats.org/officeDocument/2006/relationships/extended-properties',
+          'docProps/app.xml' ),
+        'rId2' => array(
+          'http://schemas.openxmlformats.org/package/2006/relationships/metadata/core-properties',
+          'docProps/core.xml' ),
+        'rId1' => array(
+          'http://schemas.openxmlformats.org/officeDocument/2006/relationships/officeDocument',
+          'word/document.xml' ),
+      ), $this->rels );
+    }
+
+    // Генерация зависимостей
+    protected function add_rels( $filename, $rels, $path = '' ){
+
+      // Шапка XML
+      $xmlstring = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?><Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">';
+
+      // Добавляем документы по описанным связям
+      foreach( $rels as $rId => $params ){
+
+        // Если указан путь к файлу, берем. Если нет, то берем из репозитория
+        $pathfile = empty( $params[2] ) ? $this->path . $path . $params[1] : $params[2];
+
+        // Добавляем документ в архив
+        if( $this->addFile( $pathfile ,  $path . $params[1] ) === false )
+          die('Не удалось добавить в архив ' . $path . $params[1] );
+
+        // Прописываем в связях
+        $xmlstring .= '<Relationship Id="' . $rId . '" Type="' . $params[0] . '" Target="' . $params[1] . '"/>';
+      }
+
+      $xmlstring .= '</Relationships>';
+
+      // Добавляем в архив
+      $this->addFromString( $path . $filename, $xmlstring );
+    }
+
+    public function pparse( $replace, $content ){
+
+      return str_replace( array_keys( $replace ), array_values( $replace ), $content );
+    }
+
+    // Упаковываем архив
+    public function create(){
+
+      // Добавляем типы данных
+      $this->addFile($this->path . "[Content_Types].xml" , "[Content_Types].xml" );
+
+      // Добавляем связанные документы MS Office
+      $this->add_rels( "_rels/.rels", $this->rels );
+
+      // Добавляем содержимое
+      $this->addFromString("word/document.xml", str_replace( '{CONTENT}', $this->content, file_get_contents( $this->path . "word/document.xml" ) ) );
+
+      $this->close();
+    }
+}
+
+// Класс для создания документов MS Word
+class WordDocument extends OfficeDocument{
+
+    public function __construct( $filename, $template_path = '/template/' ){
+
+      parent::__construct( $filename, $template_path );
+
+      // Описываем связи для Word
+      $this->word_rels = array(
+        "rId1" => array(
+          "http://schemas.openxmlformats.org/officeDocument/2006/relationships/styles",
+          "styles.xml"
+        ),
+        "rId2" => array(
+          "http://schemas.microsoft.com/office/2007/relationships/stylesWithEffects",
+          "stylesWithEffects.xml",
+        ),
+        "rId3" => array(
+          "http://schemas.openxmlformats.org/officeDocument/2006/relationships/settings",
+          "settings.xml",
+        ),
+        "rId4" => array(
+          "http://schemas.openxmlformats.org/officeDocument/2006/relationships/webSettings",
+          "webSettings.xml",
+        ),
+        "rId6" => array(
+          "http://schemas.openxmlformats.org/officeDocument/2006/relationships/fontTable",
+          "fontTable.xml",
+        ),
+        "rId7" => array(
+          "http://schemas.openxmlformats.org/officeDocument/2006/relationships/theme",
+          "theme/theme1.xml",
+        ),
+      );
+    }
+
+    // Регистрируем текст
+    public function assign( $content = '', $return = false ){
+
+      // Проверяем, является ли $text файлом. Если да, то подключаем изображение
+      if( is_file( $content ) ){
+
+        // Берем шаблон абзаца
+        $block = file_get_contents( $this->path . 'image.xml' );
+
+        list( $width, $height ) = getimagesize( $content );
+
+        $rid = "rId" . count( $this->word_rels ) . 'i';
+        $this->word_rels[$rid] = array(
+          "http://schemas.openxmlformats.org/officeDocument/2006/relationships/image",
+          "media/" . $content,
+
+          // Указываем непосредственно путь к файлу
+          $content
+        );
+
+        $xml = $this->pparse( array(
+          '{WIDTH}' => $width * $this->px_emu,
+          '{HEIGHT}' => $height * $this->px_emu,
+          '{RID}' => $rid,
+        ), $block );
+      }
+      else{
+
+        // Берем шаблон абзаца
+        $block = file_get_contents( $this->path . 'p.xml' );
+
+        $xml = $this->pparse( array(
+          '{TEXT}' => $content,
+        ), $block );
+      }
+
+      // Если нам указали, что нужно возвратить XML, возвращаем
+      if( $return )
+        return $xml;
+      else
+        $this->content .= $xml;
+    }
+
+    // Упаковываем архив
+    public function create(){
+
+      // Добавляем связанные документы MS Office Word
+      $this->add_rels( "_rels/document.xml.rels", $this->word_rels, 'word/' );
+
+      // Упаковываем связи с документами
+      parent::create();
+    }
+}
--- index.php
+++ index.php
@@ -1,16 +1,38 @@
 <?
 
+/**************************************
+/
+/  *** Test file for PHP Library PHPDocx ***
+/  Version: 0.9.2
+/  Author: Alexey Kichaev
+/  Home page: http://webli.ru/phpdocx/
+/  License: MIT or GPL
+/  GITHub: https://github.com/alkich/PHPDocx
+/  Date: 14/03/2012
+/
+/***************************************/
+
 // Выявляем все ошибки
 error_reporting( E_ALL | E_NOTICE );
 
 // Подключаем класс
-include 'class.word.php';
+include 'PHPDocx_0.9.2.php';
 
 // Создаем и пишем в файл. Деструктор закрывает
-$w = new Word( "Пример.docx" );
+$w = new WordDocument( "Пример.docx" );
 
-$w->assign('Пример текста.
-Будущее не предопределено.');
+// Использование метода assign
+/******************************
+/
+/ $w->assign( 'text' );
+/ $w->assign( 'image.png' );
+/ $xml = $w->assign( 'image.png', true );
+/ $w->assign( $w->assign( 'image.png' ) );
+/
+/******************************/
+
+$w->assign('image.jpg');
+$w->assign('Кто узнал эту женщину - тот настоящий знаток женской красоты.');
 
 $w->create();
 
